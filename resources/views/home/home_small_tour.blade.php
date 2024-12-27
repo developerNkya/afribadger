76b127719ce5e56f3c4164c1655157707a72398a
developernkya <developernkya@gmail.com>
2024-12-27 15:31:38 +0300

    20241227 | LOGIC FOR HOME SMALL SCREEN

---
+++ resources/views/home/home_small_tour.blade.php
@@ -0,0 +1,432 @@
+<div class="wp-block-group container_b6d8498ddcd9 is-layout-flow wp-block-group-is-layout-flow">
+    @foreach ($national_parks as $index => $tour)
+    {{-- item1 --}}
+    <div class="wp-block-group container_781276cb7f2f is-layout-flow wp-block-group-is-layout-flow">
+        <div class="wp-block-group container_9b63daa7447b is-layout-flow wp-block-group-is-layout-flow">
+            <div class="wp-block-group container_29fa4dc1b3a0 is-layout-flow wp-block-group-is-layout-flow">
+                <div class="wp-block-spacer" style="height: 0px" aria-hidden="true"></div>
+            </div>
+
+            <div class="wp-block-group container_ff68cc2413f1 is-layout-flow wp-block-group-is-layout-flow">
+                <div class="wp-block-group container_e12bb4504d98 is-layout-flow wp-block-group-is-layout-flow">
+                    <p class="text_95ecc650ae8f has-text-color has-background has-text-align-left" style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #1a1a1a99;
+background-color: transparent;
+">
+                        10 Days
+                    </p>
+
+                    <h3 class="text_be58751723d6 has-text-color has-background has-text-align-left wp-block-heading"
+                        style="
+text-transform: none;
+font-style: normal;
+font-size: 23.5px;
+font-weight: 600;
+letter-spacing: -0.5px;
+color: #26461d;
+background-color: transparent;
+">
+                        Comfort&nbsp;&nbsp;Safari&nbsp;and&nbsp;Beach&nbsp;Vacation
+                    </h3>
+                </div>
+
+                <div class="wp-block-group container_14d5c2559fd1 is-layout-flow wp-block-group-is-layout-flow">
+                    <div class="wp-block-spacer" style="height: 0px" aria-hidden="true">
+                    </div>
+                </div>
+
+                <div class="wp-block-group container_48bca086220c is-layout-flow wp-block-group-is-layout-flow">
+                    <div class="wp-block-group container_6711790e0e59 is-layout-flow wp-block-group-is-layout-flow">
+                        <p class="text_eb128dc7fa57 has-text-color has-background has-text-align-left" style="
+  text-transform: none;
+  font-style: normal;
+  font-size: 15.5px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  color: #0000008a;
+  background-color: transparent;
+">
+                            $3900
+                        </p>
+
+                        <h2 class="text_36cab543b6eb has-text-color has-background has-text-align-left wp-block-heading"
+                            style="
+  text-transform: none;
+  font-style: normal;
+  font-size: 35.5px;
+  font-weight: 600;
+  letter-spacing: -0.5px;
+  color: #26461d;
+  background-color: transparent;
+">
+                            $3300
+                        </h2>
+
+                        <div class="wp-block-group container_c64333f28d42 is-layout-flow wp-block-group-is-layout-flow">
+                            <div class="wp-block-spacer" style="height: 0px" aria-hidden="true">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="wp-block-yotako-block-anchor button_e67aaf6cd090">
+                        <a href="View_Tour/index.html" class="button_link_e67aaf6cd090" target="_self" rel="noopener">
+                            <p class="text_c78a14357f84 has-text-color has-background has-text-align-left" style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 600;
+    letter-spacing: -0.5px;
+    color: #26461d;
+    background-color: transparent;
+  ">
+                                View More
+                            </p>
+
+                            <figure class="imageview_b95dce7428f7 wp-block-image">
+                                <img decoding="async"
+                                    src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I507:2269;405:837;221:315.svg" />
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <figure class="imageview_66b6e1f40dbe wp-block-image">
+            <img decoding="async" src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/507:2270.webp" />
+        </figure>
+    </div>
+    @endforeach
+
+
+    <div class="wp-block-group container_de73eda37d3c is-layout-flow wp-block-group-is-layout-flow">
+        <p class="text_7a81fd980c72 has-text-color has-background has-text-align-left" style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 600;
+letter-spacing: -0.5px;
+color: #ffffff;
+background-color: transparent;
+">
+            Button Text
+        </p>
+
+        <figure class="imageview_4fe3ac4135f3 wp-block-image">
+            <img decoding="async"
+                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I414:3311;250:302.svg" />
+        </figure>
+    </div>
+
+    <figure class="imageview_20d5bcf66d22 wp-block-image">
+        <img decoding="async"
+            src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/414:3400.webp" />
+    </figure>
+    
+
+
+
+    {{-- mobile footer --}}
+    <div
+    class="wp-block-group container_8386b6be2e26 is-layout-flow wp-block-group-is-layout-flow">
+    <div
+        class="wp-block-group container_2b3ee51a52a9 is-layout-flow wp-block-group-is-layout-flow">
+        <div
+            class="wp-block-group container_4e25fdca1d60 is-layout-flow wp-block-group-is-layout-flow">
+            <div
+                class="wp-block-group container_8ff7270dcb35 is-layout-flow wp-block-group-is-layout-flow">
+                <figure class="imageview_428f49bd7a70 wp-block-image">
+                    <img decoding="async"
+                        src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/414:3463.svg" />
+                </figure>
+
+                <p class="text_f4d9c4a32738 has-text-color has-background has-text-align-left"
+                    style="
+  text-transform: none;
+  font-style: normal;
+  font-size: 15.5px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  color: #000000;
+  background-color: transparent;
+">
+                    Camel Leon Safaris Ltd. was born out of a love for the
+                    idea of adapting to various environments, mirroring the
+                    chameleon&apos;s ability. Our founders wanted to share
+                    this experience with people in the wilds of Africa,
+                    highlighting the beauty of nature when one immerses and
+                    adapts to it.
+                </p>
+            </div>
+        </div>
+
+        <div
+            class="wp-block-group container_ee74b7d3c5ad is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_c73e29187757 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 900;
+letter-spacing: -0.5px;
+color: #000000;
+background-color: transparent;
+">
+                Travel Tips
+            </p>
+
+            <div class="wp-block-yotako-block-anchor">
+                <a href="https://serengeticlarity.com/tanzania-entry-requirements/"
+                    class="" target="_self" rel="noopener">
+                    <p class="text_5ee0d52ddf09 has-text-color has-background has-text-align-left"
+                        style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 500;
+    text-decoration: underline;
+    letter-spacing: -0.5px;
+    color: #808080;
+    background-color: transparent;
+  ">
+                        Entry Requirements
+                    </p>
+                </a>
+            </div>
+
+            <div class="wp-block-yotako-block-anchor">
+                <a href="https://serengeticlarity.com/tanzania-safari-planning-guide-2/"
+                    class="" target="_self" rel="noopener">
+                    <p class="text_d8ef5977dac1 has-text-color has-background has-text-align-left"
+                        style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 500;
+    text-decoration: underline;
+    letter-spacing: -0.5px;
+    color: #808080;
+    background-color: transparent;
+  ">
+                        Tanzania Safari Planning Guide
+                    </p>
+                </a>
+            </div>
+
+            <div class="wp-block-yotako-block-anchor">
+                <a href="https://serengeticlarity.com/best-time-to-visit-tanzania/"
+                    class="" target="_self" rel="noopener">
+                    <p class="text_dbe539d25c06 has-text-color has-background has-text-align-left"
+                        style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 500;
+    text-decoration: underline;
+    letter-spacing: -0.5px;
+    color: #808080;
+    background-color: transparent;
+  ">
+                        Best time to visit Tanzania
+                    </p>
+                </a>
+            </div>
+
+            <div class="wp-block-yotako-block-anchor">
+                <a href="https://serengeticlarity.com/kilimanjaro-climbing-guide/"
+                    class="" target="_self" rel="noopener">
+                    <p class="text_8ed14bbd5896 has-text-color has-background has-text-align-left"
+                        style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 500;
+    text-decoration: underline;
+    letter-spacing: -0.5px;
+    color: #808080;
+    background-color: transparent;
+  ">
+                        Kilimanjaro Planning Guide
+                    </p>
+                </a>
+            </div>
+
+            <div class="wp-block-yotako-block-anchor">
+                <a href="https://serengeticlarity.com/drones-photography-guidelines/"
+                    class="" target="_self" rel="noopener">
+                    <p class="text_15561e5f10cb has-text-color has-background has-text-align-left"
+                        style="
+    text-transform: none;
+    font-style: normal;
+    font-size: 15.5px;
+    font-weight: 500;
+    text-decoration: underline;
+    letter-spacing: -0.5px;
+    color: #808080;
+    background-color: transparent;
+  ">
+                        Drones &#038; Photography Guide
+                    </p>
+                </a>
+            </div>
+            <p class="text_a9bcdb1ef504 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Tipping Guide
+            </p>
+        </div>
+
+        <div
+            class="wp-block-group container_34b4ce73d623 is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_1103452c6234 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 900;
+letter-spacing: -0.5px;
+color: #000000;
+background-color: transparent;
+">
+                Camelleon Safaris
+            </p>
+
+            <p class="text_2e5a6f0018fa has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                About US
+            </p>
+
+            <p class="text_88a67dcd7835 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Terms And Cionditions
+            </p>
+
+            <p class="text_fb1fcc3c38a6 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Booking Process
+            </p>
+
+            <p class="text_824c9002af25 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Payment Details
+            </p>
+
+            <p class="text_c9d5f9c246bf has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Privacy Policy
+            </p>
+        </div>
+
+        <div
+            class="wp-block-group container_94cce4970034 is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_b185a61e76f7 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 900;
+letter-spacing: -0.5px;
+color: #000000;
+background-color: transparent;
+">
+                Contact Information
+            </p>
+
+            <p class="text_5d1dbab7c7c6 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                Sales &#038; Safari Expert
+            </p>
+
+            <p class="text_8f2640d9b3e0 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 17.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                +25568 655 5755
+            </p>
+
+            <p class="text_eb413fd01e4d has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #808080;
+background-color: transparent;
+">
+                info@camelleonsafaris.com
+            </p>
+        </div>
+    </div>
+</div>
+<div
+class="wp-block-group container_007e9be61514 is-layout-flow wp-block-group-is-layout-flow">
+<div class="wp-block-spacer" style="height: 0px" aria-hidden="true"></div>
+</div>
+</div>
