2cebe06f930b628e7e4faea4941cd03aa189bda5
developernkya <developernkya@gmail.com>
2025-01-03 12:47:42 +0300

    20250103 | MADE NAVBAR SHARED

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,8 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="max-image-preview:large" />
@@ -1353,7 +1354,7 @@
     <link rel="alternate" title="oEmbed (XML)" type="text/xml+oembed"
         href="wp-json/oembed/1.0/embed2703?url=https%3A%2F%2Fbda1aad9-cb58-401a-8a47-526b318e3266.yotako.com%2F&amp;format=xml" />
     <link rel="stylesheet" href="{{ asset('css/home.css') }}" media="all" />
-   
+
 </head>
 
 <body class="home page-template-default page page-id-9 wp-embed-responsive">
@@ -1515,136 +1516,10 @@
                                 your trip to Tanzania a lifetime adventure.</span>
                         </p>
 
-                        <div
-                            class="wp-block-group container_a8f119c87dec is-layout-flow wp-block-group-is-layout-flow">
-                            <div
-                                class="wp-block-group container_51c3b570696f is-layout-flow wp-block-group-is-layout-flow">
-                                <figure class="imageview_c49d939a41ef wp-block-image">
-                                    <img decoding="async"
-                                        src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;219:16.svg" />
-                                </figure>
 
-                                <div
-                                    class="wp-block-group container_7102e5e4dcdf is-layout-flow wp-block-group-is-layout-flow">
-                                    <div
-                                        class="wp-block-group container_50f8551c55f1 is-layout-flow wp-block-group-is-layout-flow">
-                                        <p class="text_d5001500408f has-text-color has-background has-text-align-left"
-                                            style="
-                          text-transform: none;
-                          font-style: normal;
-                          font-size: 15.5px;
-                          font-weight: 500;
-                          letter-spacing: -0.5px;
-                          color: #26461d;
-                          background-color: transparent;
-                        ">
-                                            Home
-                                        </p>
-                                    </div>
+                        {{-- nav start --}}
+                        @include('shared.nav_bar')
 
-                                    <div
-                                        class="wp-block-group container_6402411a7f33 is-layout-flow wp-block-group-is-layout-flow">
-                                        <p class="text_bead69543599 has-text-color has-background has-text-align-left"
-                                            style="
-                          text-transform: none;
-                          font-style: normal;
-                          font-size: 15.5px;
-                          font-weight: 500;
-                          letter-spacing: -0.5px;
-                          color: #26461d;
-                          background-color: transparent;
-                        ">
-                                            Tours
-                                        </p>
-
-                                        <figure class="imageview_61bc2f878625 wp-block-image">
-                                            <img decoding="async"
-                                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;218:19;242:460.svg" />
-                                        </figure>
-                                    </div>
-
-                                    <div
-                                        class="wp-block-group container_135cac80d3d4 is-layout-flow wp-block-group-is-layout-flow">
-                                        <p class="text_086c942c373d has-text-color has-background has-text-align-left"
-                                            style="
-                          text-transform: none;
-                          font-style: normal;
-                          font-size: 15.5px;
-                          font-weight: 500;
-                          letter-spacing: -0.5px;
-                          color: #26461d;
-                          background-color: transparent;
-                        ">
-                                            Mount Trekking
-                                        </p>
-
-                                        <figure class="imageview_04347264ab57 wp-block-image">
-                                            <img decoding="async"
-                                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;218:21;242:460.svg" />
-                                        </figure>
-                                    </div>
-
-                                    <div class="wp-block-yotako-block-anchor">
-                                        <a href="About_Us/index.html" class="" target="_self" rel="noopener">
-                                            <div
-                                                class="wp-block-group container_34c8747fc0cb is-layout-flow wp-block-group-is-layout-flow">
-                                                <p class="text_3cedf42755a7 has-text-color has-background has-text-align-left"
-                                                    style="
-                              text-transform: none;
-                              font-style: normal;
-                              font-size: 15.5px;
-                              font-weight: 500;
-                              letter-spacing: -0.5px;
-                              color: #26461d;
-                              background-color: transparent;
-                            ">
-                                                    About Us
-                                                </p>
-                                            </div>
-                                        </a>
-                                    </div>
-
-                                    <div class="wp-block-yotako-block-anchor">
-                                        <a href="Contact_Us/index.html" class="" target="_self"
-                                            rel="noopener">
-                                            <div
-                                                class="wp-block-group container_df80c98a91b3 is-layout-flow wp-block-group-is-layout-flow">
-                                                <p class="text_f9e4cb89395c has-text-color has-background has-text-align-left"
-                                                    style="
-                              text-transform: none;
-                              font-style: normal;
-                              font-size: 15.5px;
-                              font-weight: 500;
-                              letter-spacing: -0.5px;
-                              color: #26461d;
-                              background-color: transparent;
-                            ">
-                                                    Contant Us
-                                                </p>
-                                            </div>
-                                        </a>
-                                    </div>
-
-                                    <div class="wp-block-yotako-block-anchor button_c63111f39e42">
-                                        <a href="index.html" class="button_link_c63111f39e42" target="_self"
-                                            rel="noopener">
-                                            <p class="text_54b6702432cf has-text-color has-background has-text-align-left"
-                                                style="
-                            text-transform: none;
-                            font-style: normal;
-                            font-size: 15.5px;
-                            font-weight: 500;
-                            letter-spacing: -0.5px;
-                            color: #26461d;
-                            background-color: transparent;
-                          ">
-                                                Book&nbsp;&nbsp;a&nbsp;Tour
-                                            </p>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                         <h2 class="text_22bf21903cae has-text-color has-background has-text-align-center wp-block-heading"
                             style="
@@ -2345,13 +2220,13 @@
 
 
 
-                        
+
 
                         {{-- mobile-footer --}}
 
 
 
-                       
+
 
                         <figure class="imageview_e402777db3d3 wp-block-image">
                             <img decoding="async"
---
+++ resources/views/shared/nav_bar.blade.php
@@ -0,0 +1,132 @@
+<div
+class="wp-block-group container_a8f119c87dec is-layout-flow wp-block-group-is-layout-flow">
+<div
+    class="wp-block-group container_51c3b570696f is-layout-flow wp-block-group-is-layout-flow">
+    <figure class="imageview_c49d939a41ef wp-block-image">
+        <img decoding="async"
+            src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;219:16.svg" />
+    </figure>
+
+    <div
+        class="wp-block-group container_7102e5e4dcdf is-layout-flow wp-block-group-is-layout-flow">
+        <a href="/" class="" target="_self" rel="noopener">
+        <div
+            class="wp-block-group container_50f8551c55f1 is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_d5001500408f has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #26461d;
+background-color: transparent;
+">
+                Home
+            </p>
+        </a>
+        </div>
+
+        <div
+            class="wp-block-group container_6402411a7f33 is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_bead69543599 has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #26461d;
+background-color: transparent;
+">
+                Tours
+            </p>
+
+            <figure class="imageview_61bc2f878625 wp-block-image">
+                <img decoding="async"
+                    src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;218:19;242:460.svg" />
+            </figure>
+        </div>
+
+        <div
+            class="wp-block-group container_135cac80d3d4 is-layout-flow wp-block-group-is-layout-flow">
+            <p class="text_086c942c373d has-text-color has-background has-text-align-left"
+                style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #26461d;
+background-color: transparent;
+">
+                Mount Trekking
+            </p>
+
+            <figure class="imageview_04347264ab57 wp-block-image">
+                <img decoding="async"
+                    src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I219:246;218:21;242:460.svg" />
+            </figure>
+        </div>
+
+        <div class="wp-block-yotako-block-anchor">
+            <a href="/about-us" class="" target="_self" rel="noopener">
+                <div
+                    class="wp-block-group container_34c8747fc0cb is-layout-flow wp-block-group-is-layout-flow">
+                    <p class="text_3cedf42755a7 has-text-color has-background has-text-align-left"
+                        style="
+  text-transform: none;
+  font-style: normal;
+  font-size: 15.5px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  color: #26461d;
+  background-color: transparent;
+">
+                        About Us
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="wp-block-yotako-block-anchor">
+            <a href="Contact_Us/index.html" class="" target="_self"
+                rel="noopener">
+                <div
+                    class="wp-block-group container_df80c98a91b3 is-layout-flow wp-block-group-is-layout-flow">
+                    <p class="text_f9e4cb89395c has-text-color has-background has-text-align-left"
+                        style="
+  text-transform: none;
+  font-style: normal;
+  font-size: 15.5px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  color: #26461d;
+  background-color: transparent;
+">
+                        Contant Us
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="wp-block-yotako-block-anchor button_c63111f39e42">
+            <a href="index.html" class="button_link_c63111f39e42" target="_self"
+                rel="noopener">
+                <p class="text_54b6702432cf has-text-color has-background has-text-align-left"
+                    style="
+text-transform: none;
+font-style: normal;
+font-size: 15.5px;
+font-weight: 500;
+letter-spacing: -0.5px;
+color: #26461d;
+background-color: transparent;
+">
+                    Book&nbsp;&nbsp;a&nbsp;Tour
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
+</div>
