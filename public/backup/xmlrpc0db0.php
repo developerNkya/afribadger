63cf341ccf37e3d8636d110d202fe5820e40a6a7
developer_nkya <developernkya@gmail.com>
2025-01-21 20:30:44 +0300

    20250125 | ADDED ASSETS FOLDER

---
+++ public/backup/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://bda1aad9-cb58-401a-8a47-526b318e3266.yotako.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
