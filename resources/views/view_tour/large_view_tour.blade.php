e042e095b7993ed6c5f0dd9423ec54d970a82bf6
developer_nkya <developernkya@gmail.com>
2025-01-27 17:41:35 +0300

    20250127 | MODIFIED ITENARY LARGE

---
+++ resources/views/view_tour/large_view_tour.blade.php
@@ -0,0 +1,72 @@
+<!-- itenary -->
+
+<div class="wp-block-group container_c9f1b11a1b51 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- ends here -->
+
+
+
+<div class="wp-block-group container_ee0a20b79ef9 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_e9c12360b1d4 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_7c1bd3296b50 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_323096f0adde is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-yotako-block-anchor button_79068482ba7e"><a href="../index.html" class="button_link_79068482ba7e" target="_self" rel="noopener">
+            
+<div class="wp-block-group container_724d47ba64cd is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+      <p class="text_0a2fe4ad23ac has-text-color has-background has-text-align-left"  style="text-transform:uppercase;font-style:normal;font-size:13.5px;font-weight:600;letter-spacing:-0.5px;color:#f4e7c9;background-color:transparent;">Day</p>
+         </a></div>
+        <h3 class="text_bf0d900ad321 has-text-color has-background has-text-align-center wp-block-heading"  style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">4</h3>
+               </div>
+
+
+<div class="wp-block-group container_d46c55d4151b is-layout-flow wp-block-group-is-layout-flow" >
+                          <h2 class="text_f6365cc971e1 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:27.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Arrival at the Airpot and transfer to Hotel</h2>
+               </div>
+        </div>
+
+
+
+<figure class="imageview_d207cb065864 wp-block-image" >
+<img decoding="async"  src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I285:861;280:381.svg" />
+</figure>
+
+        </div>
+        </div>
+        </div>
+
+
+ <!-- stick -->
+<!--  <div class="wp-block-group container_ed0f18246d7b is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+ -->
