70f770c79da8f827284765d8011944483b5a03b8
developer_nkya <developernkya@gmail.com>
2025-01-30 20:44:19 +0300

    20250128 | MOVED FAQ SECTION-LARGE

--- resources/views/view_tour/large_view_tour.blade.php
+++ resources/views/view_tour/large_view_tour.blade.php
@@ -117,45 +117,59 @@
 
 
 <script>
- document.addEventListener("DOMContentLoaded", function () {
-  // Select all image elements that trigger the display of the itinerary
+document.addEventListener("DOMContentLoaded", function () {
+  // Itinerary logic (remains unchanged)
   const imageViews = document.querySelectorAll('.imageview_d207cb065864');
-
-  // Select all button-expandable elements for hiding the itinerary
   const expandableButtons = document.querySelectorAll('.button-expandable');
 
-  // Iterate over each image and its corresponding expandable button
   imageViews.forEach((imageView, index) => {
-    // Find the closest parent container of the clicked image that contains the itinerary
     const parentContainer = imageView.closest('.wp-block-group.container_c9f1b11a1b51');
-    
-    // Find the .opened-itenary inside this parent container
     const itinerary = parentContainer ? parentContainer.querySelector('.opened-itenary') : null;
 
-    // If itinerary is not found, log an error
     if (!itinerary) {
       console.error('Itinerary not found for the current image view.');
       return;
     }
 
-    // Find the expandable button that corresponds to the current itinerary
     const expandableButton = expandableButtons[index];
 
-    // Show the itinerary when the image is clicked
     imageView.addEventListener('click', function () {
-      itinerary.classList.add('show'); // Show the itinerary
-      expandableButton.src = '../../assets/images/button-expandable.png'; // Change to minus icon
+      itinerary.classList.add('show'); 
+      expandableButton.src = '../../assets/images/button-expandable.png'; 
     });
 
-    // Hide the itinerary when the expandable (plus) button is clicked
     expandableButton.addEventListener('click', function () {
-      itinerary.classList.remove('show'); // Hide the itinerary
-      expandableButton.src = '../../assets/images/button-expandable.png'; // Change back to plus icon
+      itinerary.classList.remove('show'); 
+      expandableButton.src = '../../assets/images/button-expandable.png'; 
+    });
+  });
+
+  // FAQ toggle logic (new functionality)
+    const faqView = document.querySelectorAll('.imageview_d207cb065864-faq');
+  const faqButtons = document.querySelectorAll('.button-expandable-faq');
+
+  faqView.forEach((imageView, index) => {
+    const parentContainer = imageView.closest('.wp-block-group.container_c9f1b11a1b51');
+    const itinerary = parentContainer ? parentContainer.querySelector('.opened-itenary-faq') : null;
+
+    if (!itinerary) {
+      console.error('Itinerary not found for the current image view.');
+      return;
+    }
+
+    const faqButton = faqButtons[index];
+
+    imageView.addEventListener('click', function () {
+      itinerary.classList.add('show'); 
+      faqButton.src = '../../assets/images/button-expandable.png'; 
+    });
+
+    faqButton.addEventListener('click', function () {
+      itinerary.classList.remove('show'); 
+    faqButton.src = '../../assets/images/button-expandable.png'; 
     });
   });
 });
-
-
 </script>
 
 
@@ -316,36 +330,112 @@
         </div>
 </div>
 
+
+<!-- faq section -->
+    <h2 class=" faq-title text_ce43d4354680-faq has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">FAQ</h2>
 <div class="faq-section">
-  <div class="wp-block-group container_a4a629c28842 is-layout-flow wp-block-group-is-layout-flow" >
-                          <h2 class="text_769751442867 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">FAQ’s</h2>
-       
-
-<div class="wp-block-group container_02b5b2cfe07b is-layout-flow wp-block-group-is-layout-flow" >
-   
-      
-        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
-        
+  <div class="wp-block-group container_ee0a20b79ef9 is-layout-flow wp-block-group-is-layout-flow">
+  <!-- Inside structure -->
+  <div class="wp-block-group container_e9c12360b1d4 is-layout-flow wp-block-group-is-layout-flow" style="width:1117px">
+    <div class="wp-block-group container_7c1bd3296b50 is-layout-flow wp-block-group-is-layout-flow">
+      <div class="wp-block-group container_d46c55d4151b is-layout-flow wp-block-group-is-layout-flow">
+        <h2 class="text_f6365cc971e1 has-text-color has-background has-text-align-left wp-block-heading" style="position:relative;text-transform:none;font-style:normal;font-size:27.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Arrival at the Airport and transfer to Hotel</h2>
+      </div>
     </div>
+    
+    <!-- Clickable Image -->
+    <figure class="imageview_d207cb065864-faq wp-block-image">
+      <img decoding="async" src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/196:500.svg" />
+    </figure>
+  </div>
 
 
-
-
-
-
-        <h2 class="text_b3cfa3233881 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:27.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">What safety measures should I be aware of during a safari in Tanzania?</h2>
-       
-
-       
-
-
-<figure class="imageview_9b77e4d14912 wp-block-image" >
-<img decoding="async"  src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/196:500.svg" />
-</figure>
-
-
+  <!-- Itinerary (Initially Hidden) -->
+ <!-- Itinerary (Initially Hidden) -->
+<!-- Itinerary (Initially Hidden) -->
+<div class="opened-itenary-faq" style="margin-top: 50px; background-color: white; padding-top: 20px;">
+  <div class="state-yes">
+    <div class="collapsible-button">
+      <div class="frame">
+        <div class="day-count">
+          <div class="div">
+            <div class="rectangle"></div>
+            <div class="day">DAY</div>
+          </div>
+          <div class="text-wrapper">1</div>
         </div>
+        <div class="div-wrapper">
+          <p class="p">Arrival at the Airport and transfer to Hotel</p>
+        </div>
+      </div>
+      <img class="button-expandable-faq" src="../../assets/images/button-expandable.png" />
+    </div>
+    <div class="rectangle-2"></div>
+    <div class="frame-2">
+      <img class="group" src="../../assets/images/group-22.png" />
+      <div class="frame-3">
+        <div class="text-wrapper-2">Arrival</div>
+        <p class="the-group-arrives-at">
+          The group arrives at Kilimanjaro Airport (JRO). Participants are met by a representative of Altezza Travel and transferred to a hotel in Arusha.<br />&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp;&nbsp;&nbsp;&nbsp;Note: The hotel cost only includes breakfast. Check-in starts at 2:00 PM.
+        </p>
+      </div>
+    </div>
+    <div class="frame-4">
+      <div class="group-wrapper">
+        <div class="overlap-wrapper">
+          <div class="overlap">
+            <div class="overlap-group">
+              <div class="rectangle-3"></div>
+              <div class="rectangle-4"></div>
+              <div class="rectangle-5"></div>
+            </div>
+            <div class="rectangle-6"></div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-5">
+        <div class="frame-6">
+          <div class="text-wrapper-3">Accommodation</div>
+          <div class="text-wrapper-4">GRAND MELIA ARUSHA</div>
+        </div>
+        <div class="frame-7">
+          <img class="img" src="../../assets/images/rectangle-71.png" /> <img class="img" src="../../assets/images/rectangle-72.png" />
+        </div>
+        <div class="frame-8">
+          <div class="frame-9">
+            <img class="vector" src="img/vector-2.svg" />
+            <div class="text-wrapper-5">Meal Plan:</div>
+          </div>
+          <div class="text-wrapper-6">Breakfast included</div>
+        </div>
+        <div class="frame-10">
+          <div class="frame-11">
+            <img class="vector-2" src="img/image.svg" />
+            <div class="text-wrapper-7">Swimming Pool</div>
+          </div>
+          <div class="frame-11">
+            <img class="vector-2" src="img/vector.svg" />
+            <div class="text-wrapper-7">Wifi</div>
+          </div>
+          <div class="frame-11">
+            <img class="vector-2" src="img/vector-3.svg" />
+            <div class="text-wrapper-7">Restaurant</div>
+          </div>
+          <div class="frame-11">
+            <img class="vector-2" src="img/vector-4.svg" />
+            <div class="text-wrapper-7">Spa</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+
+</div>
+</div>
+
+
         </div>
 
 
