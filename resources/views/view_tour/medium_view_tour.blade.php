338c2c84cbf3ef37e36ba9f3b29a783d598eb077
developer_nkya <developernkya@gmail.com>
2025-02-02 00:20:05 +0300

    20250102 | PREVENTED FOOTER FROM DUBLICATE

---
+++ resources/views/view_tour/medium_view_tour.blade.php
@@ -0,0 +1,43 @@
+<div class="wp-block-group container_da6f6efcc8ce is-layout-flow wp-block-group-is-layout-flow" >
+                  
+
+
+
+
+<div class="wp-block-group container_05a35a35cbc2 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_2608d335926e is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_94ba8e6dae40 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_63a4225468d7 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-yotako-block-anchor button_63fee80ead3f"><a href="../index.html" class="button_link_63fee80ead3f" target="_self" rel="noopener">
+            
+<div class="wp-block-group container_4b3c3f87d48e is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+      <p class="text_1f4d1cf86082 has-text-color has-background has-text-align-left"  style="text-transform:uppercase;font-style:normal;font-size:13.5px;font-weight:600;letter-spacing:-0.5px;color:#f4e7c9;background-color:transparent;">Day</p>
+         </a></div>
+        <h3 class="text_dbd8059627d9 has-text-color has-background has-text-align-center wp-block-heading"  style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">4</h3>
+               </div>
+
+
+<div class="wp-block-group container_d6f91a95ee8e is-layout-flow wp-block-group-is-layout-flow" >
+                          <h2 class="text_fa55cf660874 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:27.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Depature</h2>
+               </div>
+        </div>
+
+
+
+<figure class="imageview_d810f0f30684 wp-block-image" >
+<img decoding="async"  src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/I419:4155;280:381.svg" />
+</figure>
+
+        </div>
+        </div>
+        </div>
