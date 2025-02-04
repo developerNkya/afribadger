126b13119c9fefb68567c2c0b73e31ca915e28bc
developer_nkya <developernkya@gmail.com>
2025-02-04 18:51:05 +0300

    20250204 | HANDLED ITENARY IN SMALL SCREEN

---
+++ resources/views/view_tour/small_view_tour.blade.php
@@ -0,0 +1,342 @@
+<div class="wp-block-group container_4e3d9e39666e is-layout-flow wp-block-group-is-layout-flow" >
+                          <h2 class="text_ff42df684304 has-text-color has-background has-text-align-center wp-block-heading"  style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Overview</h2>
+       
+        <h3 class="text_753ef0810df9 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">This extraordinary journey takes you deep into the heart of Ngorongoro Crater, a volcanic caldera over two million years old. Renowned for its unparalleled wildlife density, Ngorongoro is home to the majestic Big Five—lion, elephant, buffalo, rhino, and leopard—alongside zebras, wildebeests, gazelles, and hippos.Your adventure begins as you descend into the crater, surrounded by its steep, lush walls. <br/><br/>The floor of the crater is a dynamic mix of savannah, swamps, and lakes, creating a vibrant ecosystem teeming with life. Here, you will witness the raw beauty and incredible diversity of Tanzania’s wildlife in their natural habita. As you traverse the crater, you’ll move through tropical forests and semi-deserts, each step revealing new facets of this ancient land. <br/><br/>The rich volcanic soil supports a stunning array of flora and fauna, making every moment a unique and unforgettable experience. Surely, these memories of the Ngorongoro Crater, with its awe-inspiring landscapes and abundant wildlife, will stay with you forever. Join us for a safari that promises not just a glimpse, but a full immersion into the wild world of Tanzania</h3>
+
+
+<!-- border -->
+<div class="wp-block-group container_38788649a70b is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+    <!-- itenary -->
+  <div class="itenary-small">
+    <h2 class="text_da06bcbaabd7 has-text-color has-background has-text-align-left wp-block-heading" 
+        style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+        Itinerary
+    </h2>
+
+    <div class="wp-block-yotako-block-anchor button_918fe2cd01a0">
+      <div class="wp-block-group container_7e3530d3246e is-layout-flow wp-block-group-is-layout-flow">
+        <div class="wp-block-group container_f3bfadad181a is-layout-flow wp-block-group-is-layout-flow">
+          <div class="wp-block-group container_9325242a4eaa is-layout-flow wp-block-group-is-layout-flow">
+            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+          </div>
+
+          <p class="text_760c0f94c923 has-text-color has-background has-text-align-left" 
+             style="text-transform:uppercase;font-style:normal;font-size:13.5px;font-weight:600;letter-spacing:-0.5px;color:#f4e7c9;background-color:transparent;">
+             Day
+          </p>
+        </div>
+
+        <h3 class="text_5a947dcac893 has-text-color has-background has-text-align-center wp-block-heading" 
+            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+            1
+        </h3>
+      </div>
+
+      <div class="wp-block-group container_5f66b41bcc64 is-layout-flow wp-block-group-is-layout-flow">
+        <p class="text_c273670e1c70 has-text-color has-background has-text-align-left" 
+           style="text-transform:none;font-style:normal;font-size:15.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+           Arrival at the Airport and transfer to Hotel
+        </p>
+      </div>
+
+      <figure class="imageview_7690f9900705 wp-block-image">
+        <img decoding="async" src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/423:4760.svg" />
+      </figure>
+    </div>
+
+    <!-- when opened:: -->
+    <div class="small-tour-details">
+      <div class="opened-itenary" style="background-color: white; padding-top: 20px;">
+        <div class="state-yes" style="height:fit-content">
+          <div class="collapsible-button">
+            <div class="frame">
+              <div class="day-count">
+                <div class="div">
+                  <div class="rectangle"></div>
+                  <div class="day">DAY</div>
+                </div>
+                <div class="text-wrapper">1</div>
+              </div>
+              <div class="div-wrapper">
+                <p class="p">Arrival at the Airport and transfer to Hotel</p>
+              </div>
+            </div>
+            <img class="button-expandable-small" src="../../assets/images/button-expandable.png" />
+          </div>
+
+          <div class="rectangle-2"></div>
+          <div class="frame-2">
+            <img class="group" src="../../assets/images/group-22.png" />
+            <div class="frame-3">
+              <div class="text-wrapper-2">Arrival</div>
+              <p class="the-group-arrives-at">
+                The group arrives at Kilimanjaro Airport (JRO). Participants are met by a representative of Altezza Travel and transferred to a hotel in Arusha.<br />
+                Note: The hotel cost only includes breakfast. Check-in starts at 2:00 PM.
+              </p>
+            </div>
+          </div>
+          <div class="frame-4">
+            <div class="group-wrapper">
+              <div class="overlap-wrapper">
+                <div class="overlap">
+                  <div class="overlap-group">
+                    <div class="rectangle-3"></div>
+                    <div class="rectangle-4"></div>
+                    <div class="rectangle-5"></div>
+                  </div>
+                  <div class="rectangle-6"></div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-5">
+              <div class="frame-6">
+                <div class="text-wrapper-3">Accommodation</div>
+                <div class="text-wrapper-4">GRAND MELIA ARUSHA</div>
+              </div>
+              <div class="frame-7">
+                <img class="img" src="../../assets/images/rectangle-71.png" />
+                <img class="img" src="../../assets/images/rectangle-71.png" />
+              </div>
+              <div class="frame-8">
+                <div class="frame-9">
+                  <img class="vector" src="img/vector-2.svg" />
+                  <div class="text-wrapper-5">Meal Plan:</div>
+                </div>
+                <div class="text-wrapper-6">Breakfast included</div>
+              </div>
+              <div class="frame-10">
+                <div class="frame-11">
+                  <img class="vector-2" src="img/image.svg" />
+                  <div class="text-wrapper-7">Swimming Pool</div>
+                </div>
+                <div class="frame-11">
+                  <img class="vector-2" src="img/vector.svg" />
+                  <div class="text-wrapper-7">Wifi</div>
+                </div>
+                <div class="frame-11">
+                  <img class="vector-2" src="img/vector-3.svg" />
+                  <div class="text-wrapper-7">Restaurant</div>
+                </div>
+                <div class="frame-11">
+                  <img class="vector-2" src="img/vector-4.svg" />
+                  <div class="text-wrapper-7">Spa</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+  // Itinerary logic
+  const imageViews = document.querySelectorAll('.imageview_7690f9900705');
+  const expandableButtons = document.querySelectorAll('.button-expandable-small');
+
+  imageViews.forEach((imageView, index) => {
+    // Find the parent container and the opened itinerary
+    const parentContainer = imageView.closest('.wp-block-group .itenary-small');
+    const itinerary = parentContainer ? parentContainer.querySelector('.opened-itenary') : null;
+
+    if (!itinerary) {
+      console.error('Itinerary not found for the current image view.');
+      return;
+    }
+
+    const expandableButton = expandableButtons[index];
+
+    imageView.addEventListener('click', function () {
+      console.log("disable1");
+      itinerary.classList.add('show');
+      expandableButton.src = '../../assets/images/button-expandable.png';
+    });
+
+    expandableButton.addEventListener('click', function () {
+      console.log("disable");
+      itinerary.classList.remove('show');
+      expandableButton.src = '../../assets/images/button-expandable.png';
+    });
+  });
+
+  // FAQ toggle logic
+//   const faqView = document.querySelectorAll('.imageview_d810f0f30684-faq');
+//   const faqButtons = document.querySelectorAll('.button-expandable-faq-m');
+
+//   faqView.forEach((imageView, index) => {
+//     const parentContainer = imageView.closest('.wp-block-group');
+//     const itinerary = parentContainer ? parentContainer.querySelector('.opened-itenary-faq') : null;
+
+//     if (!itinerary) {
+//       console.error('Itinerary not found for the current image view.');
+//       return;
+//     }
+
+//     const faqButton = faqButtons[index];
+
+//     imageView.addEventListener('click', function () {
+//       itinerary.classList.add('show');
+//       itinerary.style.display = 'inline'; // Set display to inline after opening
+//       faqButton.src = '../../assets/images/button-expandable.png';
+//     });
+
+//     faqButton.addEventListener('click', function () {
+//       itinerary.classList.remove('show');
+//       itinerary.style.display = 'none'; // Hide the FAQ when clicked again
+//       faqButton.src = '../../assets/images/button-expandable.png';
+//     });
+//   });
+ });
+
+</script>
+
+
+
+<!-- safari includes -->
+<div class="safari-includes-small">
+       <div class="wp-block-group container_c3d225d0f727 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_2fde2e0b36f6 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_1e14ebc3dd51 is-layout-flow wp-block-group-is-layout-flow" >
+                          <h2 class="text_72898d924eee has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Safari Cost Includes</h2>
+       
+
+<div class="wp-block-group container_da3fadf1eb23 is-layout-flow wp-block-group-is-layout-flow" >
+                        <p class="text_9a2c731ab1ec has-text-color has-background has-text-align-left"  style="text-transform:none;font-style:normal;font-size:15.5px;font-weight:400;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Full board accommodation as specified on safari.<br/>4WD Land Cruiser vehicle with pop-up roof.<br/>Well-trained driver-guide.<br/>Full board meals whilst on safari.<br/>All the national park fees as per itinerary.<br/>All meals whilst on safari.<br/>Mineral water.</p>
+     
+
+<div class="wp-block-group container_de2323cc471f is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_b50c36e49edb is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_b5ed007522fe is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_735c85501449 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_b2f51c13ffe7 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_d2d0e18afb16 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_d6c8eea5cf22 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+        </div>
+        </div>
+
+
+<div class="wp-block-group container_6960a4e248ca is-layout-flow wp-block-group-is-layout-flow" >
+                          <h2 class="text_d2c0d88dd7b7 has-text-color has-background has-text-align-left wp-block-heading"  style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Safari Cost Excludes</h2>
+       
+
+<div class="wp-block-group container_abca94120be3 is-layout-flow wp-block-group-is-layout-flow" >
+                  
+<div class="wp-block-group container_70e2e28de0dc is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_2c9f64320d65 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_3ae3aec5c038 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_1188c0a3d03f is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_f5b32fa7f580 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_32aff9fc6389 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+
+<div class="wp-block-group container_c60d99a63de5 is-layout-flow wp-block-group-is-layout-flow" >
+   
+      
+        <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div> 
+        
+    </div>
+
+      <p class="text_889812c376b6 has-text-color has-background has-text-align-left"  style="text-transform:none;font-style:normal;font-size:15.5px;font-weight:400;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">Airfares airport taxes and fees.<br/>Laundry.<br/>Entry visas and vaccinations.<br/>Personal items.<br/>Tips and gratuities.<br/>Travel Insurance.<br/>Optional tour.<br/>Guide tips and any items of personal nature.</p>
+             </div>
+        </div>
+        </div>
+        </div>
+</div>
+
+               </div>
