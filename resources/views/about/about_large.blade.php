d1b1220b8898a227e73730f6a377fae5d68fabe2
developernkya <developernkya@gmail.com>
2025-02-26 06:02:36 -0800

    20250226 | ADDED IMAGES ON LARGE -ABOUT

---
+++ resources/views/about/about_large.blade.php
@@ -0,0 +1,256 @@
+<div
+                        class="vp_1440 size_1440 viewport_46e7d6bdea22 wp-block-group yotako-main is-layout-flow wp-block-group-is-layout-flow">
+
+
+                        <figure class="imageview_deb16fe5a1ac wp-block-image">
+                            <img decoding="async"
+                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/200:735.webp" />
+                        </figure>
+
+
+
+                        <div class="wp-block-group container_f069ba8e5373 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+                        <h2 class="text_5f7521613ea7 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:63.5px;font-weight:600;letter-spacing:-0.5px;color:#f4e7c9;background-color:transparent;">
+                            About Us</h2>
+
+                        <h2 class="text_951b6c143ce5 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:400;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Who we are</h2>
+
+                        <div class="our-team-large wp-block-group is-layout-flow wp-block-group-is-layout-flow">
+                        <h2 class="text_b150f8b14b80 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:400;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Meet Our Team</h2>
+
+                            <h3 class="text_380687dd0a25 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            At Camel Leon Safaris Ltd., our team is the heart of our company, each member bringing their
+                            passion, expertise, and dedication to ensure you have an unforgettable adventure.</h3>
+                        
+                            <div class="planners">
+
+                            </div>
+                            <h2 class="text_c20d8ecf8cad has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Travel Planners</h2>
+                       
+                        </div>
+                    
+                        
+
+
+                        
+
+                        <h2 class="text_bb412923569f has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:47.5px;font-weight:400;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Our Commitment</h2>
+
+
+
+                        <div
+                            class="wp-block-group container_161101829b88 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+
+                        <div
+                            class="wp-block-group container_4d8ed1787f4a is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+                        
+
+                        <h2 class="text_f1f9e4efbf06 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Sustainability Experts</h2>
+
+                        <h2 class="text_79ae1677166a has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Customer Support</h2>
+
+                        <h2 class="text_a16a2f95ca50 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Logistics Team</h2>
+
+
+                        <h3 class="text_421f5521569e has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Welcome to Camel Leon Safaris Ltd., where adventure meets adaptation. Inspired by the
+                            chameleon’s ability to thrive in diverse environments, our founders created Camel Leon
+                            Safaris to share the unparalleled beauty of Tanzania’s wild landscapes with explorers from
+                            around the world. Our journey began with a deep love for nature and a commitment to
+                            showcasing Tanzania’s rich biodiversity. We specialise in crafting bespoke safari
+                            experiences that connect you with the heart of nature. From thrilling wildlife encounters to
+                            immersive cultural tours, our adventures are designed to inspire and rejuvenate</h3>
+
+                        <h3 class="text_535604441eff has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Our dedicated travel planners work tirelessly to create personalized itineraries tailored to
+                            your preferences. They meticulously plan every detail, from accommodations to activities,
+                            ensuring a seamless and memorable experience. Their commitment to excellence ensures that
+                            your safari is uniquely yours.</h3>
+
+                        <h3 class="text_b1353b77002b has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Our sustainability experts focus on preserving the natural beauty of Tanzania. They
+                            implement eco-friendly practices in our operations and work with local communities to
+                            promote conservation and sustainable tourism. Their efforts help us protect the environment
+                            and support local livelihoods.</h3>
+
+                        <h3 class="text_a0ebe49073fd has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Our customer support team is always ready to assist you with any inquiries or needs you may
+                            have. They are dedicated to providing prompt, friendly, and efficient service, making sure
+                            your experience with Camel Leon Safaris is smooth from start to finish.</h3>
+
+                        <h3 class="text_70dcb68bbfa3 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Behind every great safari is a skilled logistics team ensuring everything runs smoothly.
+                            From transportation to accommodations, they handle all the behind-the-scenes details,
+                            allowing you to focus on enjoying your adventure.</h3>
+
+                        <h3 class="text_a98ad495dc5f has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            Together, our team at Camel Leon Safaris Ltd. is committed to providing you with an
+                            extraordinary safari experience, filled with awe-inspiring moments and lifelong memories.
+                            Join us and discover the wonders of Tanzania with a team that truly cares.</h3>
+
+                            <figure class="imageview_deb16fe5a1ac_commitment wp-block-image">
+                            <img decoding="async"
+                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/200:735.webp" />
+                        </figure>
+
+
+                        <div class="view-tour-large-footer" style="position: relative;top:4500px;">
+   @include('shared.footer');
+</div>              
+
+                        <h3 class="text_8edc68d0c5c7 has-text-color has-background has-text-align-left wp-block-heading"
+                            style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                            At Camel Leon Safaris, we prioritise creating exceptional, personalised experiences for our
+                            clients. Our expert guides have extensive knowledge of local culture and attractions,
+                            ensuring every safari is enriching and unique. With a focus on sustainable tourism
+                            practices, we aim to preserve the natural beauty of Tanzania for future generations.</h3>
+
+
+
+                        <figure class="imageview_7574ef584676 wp-block-image">
+                            <img decoding="async"
+                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/200:691.svg" />
+                        </figure>
+
+
+
+                        <div
+                            class="wp-block-group container_f71ba21ea935 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+
+                        <div
+                            class="wp-block-group container_1433a7313d80 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+
+
+                        <figure class="imageview_c1a0c59d06f9 wp-block-image">
+                            <img decoding="async"
+                                src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/204:792.webp" />
+                        </figure>
+
+
+
+                        <div
+                            class="wp-block-group container_b901908219cd is-layout-flow wp-block-group-is-layout-flow">
+
+                            <div
+                                class="wp-block-group container_6be2f7305802 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                                <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                            </div>
+
+
+
+                            <figure class="imageview_7d91c2b4898a wp-block-image">
+                                <img decoding="async"
+                                    src="https://cdn.yotako.io/95521a4f-a0a8-413a-a79e-c14ca627a987/204:859.webp" />
+                            </figure>
+
+                        </div>
+
+
+                        <!-- <div
+                            class="wp-block-group container_0c84d11c1ac2 is-layout-flow wp-block-group-is-layout-flow">
+
+                            <div
+                                class="wp-block-group container_524f18cc61f4 is-layout-flow wp-block-group-is-layout-flow">
+                                <h2 class="text_863d2774e9df has-text-color has-background has-text-align-left wp-block-heading"
+                                    style="text-transform:none;font-style:normal;font-size:31.5px;font-weight:600;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                                    Founders</h2>
+
+                                <h3 class="text_0e4c81cdc0fa has-text-color has-background has-text-align-left wp-block-heading"
+                                    style="text-transform:none;font-style:normal;font-size:19.5px;font-weight:500;letter-spacing:-0.5px;color:#26461d;background-color:transparent;">
+                                    Our founders, inspired by the chameleon’s adaptability, envisioned creating safari
+                                    experiences that allow travelers to immerse themselves in the beauty of Tanzania.
+                                    Their vision and commitment to sustainable tourism have shaped Camel Leon Safaris
+                                    into what it is today.</h3>
+                            </div>
+
+
+                            <div
+                                class="wp-block-group container_56dca788d0c1 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                                <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                            </div>
+                        </div> -->
+
+
+                        <div
+                            class="wp-block-group container_5aeaa57cfc69 is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+
+                        <div
+                            class="wp-block-group container_e327b71cd36e is-layout-flow wp-block-group-is-layout-flow">
+
+
+                            <div class="wp-block-spacer" style="height:0px" aria-hidden="true"></div>
+
+                        </div>
+
+
+                        {{-- nav-start --}}
+                        <div class="about-nav-bar" style="top:30px;position:absolute;">
+                            @include('shared.nav_bar')
+                        </div>
+                       
+
+                    </div>
